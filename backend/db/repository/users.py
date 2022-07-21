047db663d0dc7a103ae6c9253496226c4376cab7
SokolovJek <sokolovgek@mail.ru>
2022-07-21 16:09:18 +0300

    добавил post запрос на создание пользователя. Обработал ответ сервера на успешный POST, сократил ответ(username, password, is_active)

---
+++ backend/db/repository/users.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+
+from schemas.users import UserCreate
+from db.models.users import User
+from core.hashing import Hasher
+
+
+def create_new_user(user: UserCreate, db: Session):
+    """
+    Извлекли логику работы с БД из функции create_user(end-point '/user/'),
+     для того чтоб в случае чего можно было изменить ORM
+    """
+    user = User(username=user.username,
+                email=user.email,
+                hashed_password=Hasher.get_password_hash(user.password),
+                is_active=True,
+                is_superuser=False
+                )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -2,18 +2,25 @@ from pydantic import BaseModel, EmailStr
 from typing import Optional
 
 
-# свойства, необходимые при создании пользователя.
+# указываем свойства, необходимые при создании пользователя.
 class UserCreate(BaseModel):
+    """
+    Валидация данных
+    """
     username: str
     email: EmailStr
     password: str
 
 
-a = UserCreate
-a.username = 'dsd'
-print(a.username)
-a.username = 1
-print(type(a.username))
+# модель ответа сервера на запрос 'POST http://127.0.0.1:8000/users/'
+class ShowUser(BaseModel):
+    """
+    Явно указываем что будет возвращенно пользователю.
+    Убираем и ответа поля: hash_password и is_superuser
+    """
+    username: str
+    email: EmailStr
+    is_active: bool
 
-a.email = 1111
-print(a.email)
+    class Config:
+        orm_mode = True
