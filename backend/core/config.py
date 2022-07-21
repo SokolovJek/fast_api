e92b699e2beab42f5cedb0b3401eb77b4c5e915b
SokolovJek <sokolovgek@mail.ru>
2022-07-21 10:10:57 +0300

    сделал стартовый проект, добавил jinja2 для красоты)

---
+++ backend/core/config.py
@@ -0,0 +1,6 @@
+class Settings:
+    PROJECT_NAME: str = 'SOKOLOV EVGENIY'
+    PROJECT_VERSION: str = "1.0.0"
+
+
+settings = Settings()
---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from core.config import settings
+from apis.general_pages.route_homepage import general_pages_router
+
+
+def include_router(app):
+    app.include_router(general_pages_router)
+
+
+def start_applications():
+    my_app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    include_router(my_app)
+    return my_app
+
+
+app = start_applications()
+
+# @app.get("/")
+# def hello_api():
+#     return {"msg": "Hello API"}
