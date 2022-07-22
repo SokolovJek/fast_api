36de42367150775a1bcf272eb831777f919cc727
SokolovJek <sokolovgek@mail.ru>
2022-07-22 19:28:26 +0300

    добавлена поддержка авторизации, добавленна авторизация при доступе к определеннм end-poit(jobs/delite, jobs/create)

--- backend/apis/version1/route_login.py
+++ backend/apis/version1/route_login.py
@@ -1,8 +1,9 @@
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
 from datetime import timedelta
 from fastapi import status, HTTPException
+from jose import JWTError, jwt
 
 from db.session import get_db
 from core.hashing import Hasher
@@ -43,3 +44,29 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
         data={"sub": user.email}, expires_delta=access_token_expire
     )
     return {'access_token': access_token, 'token_type': 'bearer'}
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/login/token')
+
+
+def get_current_user_form_token(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    """
+    Она работает как зависимость,
+    создадим зависимость для идентификации current_user.
+    :param token: токен
+    :param db: объект БД
+    :return: объект пользователя
+    """
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail='Не удалось подтвердить учетные данные')
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get('sub')
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=username, db=db)
+    if user is None:
+        raise credentials_exception
+    return user
